7fe7bef7ea733584d0612c9a22eca225c1e78872
SumanthMedavarapu <60243899+SumanthMedavarapu@users.noreply.github.com>
2020-06-09 21:38:25 -0600

    .

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+first_number = int(input("enter first number"))
+second_number = int(input("enter second number"))
+print('Addition',first_number + second_number)
+print('Subtraction',first_number-second_number)
+
+
+print('Multiplication ',first_number * second_number)
+print('Division  ',first_number / second_number)
+print('Modulus   ',first_number % second_number)
+print('Exponentiation   ',first_number ** second_number)
+print('Floor  ',first_number // second_number)
+
---
+++ reversestring.py
@@ -0,0 +1,3 @@
+user_input = input("Enter the string from user")
+removed_string = user_input[:-2]
+print(removed_string[::-1])
